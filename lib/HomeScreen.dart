cadcfec87f88f43fba17b33acbfb99b054a201e1
Avinash175 <avinashmagar15@gmail.com>
2022-11-19 21:13:18 +0530

    New Changes Done

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
 import 'package:cryto_currency_app/app_theme.dart';
 import 'package:cryto_currency_app/update_profile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'package:cryto_currency_app/coin_details_model.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,12 +14,16 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String url =
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false";
   String name = "", age = " ", email = "";
   bool isDarkMode = AppTheme.isDarkModenabled;
+  GlobalKey<ScaffoldState> _globalKey = GlobalKey<ScaffoldState>();
 
   void initState() {
     super.initState();
     getUserDetails();
+    getCoinDetails();
   }
 
   void getUserDetails() async {
@@ -28,15 +35,42 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  Future<List<CoinDetailsModel>> getCoinDetails() async {
+    Uri uri = Uri.parse(url);
+
+    final response = await http.get(uri);
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      List coinsData = json.decode(response.body);
+
+      List<CoinDetailsModel> data =
+          coinsData.map((e) => CoinDetailsModel.fromJson(e)).toList();
+      return data;
+    } else {
+      return <CoinDetailsModel>[];
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _globalKey,
       backgroundColor: isDarkMode ? Colors.black : Colors.white,
       appBar: AppBar(
-        leading: (IconButton(onPressed: (){},icon: Icon(Icons.menu,color: Colors.black,),)),
+        leading: (IconButton(
+          onPressed: () {
+            _globalKey.currentState!.openDrawer();
+          },
+          icon: Icon(
+            Icons.menu,
+            color: Colors.black,
+          ),
+        )),
         backgroundColor: Colors.white,
         elevation: 0,
-        title: Text("Crpto Currency App",style: TextStyle(color: Colors.black),),
+        title: Text(
+          "Crpto Currency App",
+          style: TextStyle(color: Colors.black),
+        ),
         centerTitle: true,
       ),
       drawer: Drawer(
@@ -79,13 +113,13 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
             ListTile(
-              onTap: () async{
-                SharedPreferences prefs=await SharedPreferences.getInstance();
+              onTap: () async {
+                SharedPreferences prefs = await SharedPreferences.getInstance();
 
                 setState(() {
                   isDarkMode = !isDarkMode;
                 });
-                AppTheme.isDarkModenabled=isDarkMode;
+                AppTheme.isDarkModenabled = isDarkMode;
                 await prefs.setBool('isDarkMode', isDarkMode);
               },
               leading: Icon(
@@ -102,6 +136,77 @@ class _HomeScreenState extends State<HomeScreen> {
             )
           ],
         ),
+      ),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+                  hintText: "Search For Coin",
+                ),
+              ),
+            ),
+            Expanded(
+              child: FutureBuilder(
+                  future: getCoinDetails(),
+                  builder: (context,
+                      AsyncSnapshot<List<CoinDetailsModel>> snapshot) {
+                    if (snapshot.hasData) {
+                      return ListView.builder(
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) {
+                            return coinDetails(snapshot.data![index]);
+                          });
+                    } else {
+                      return Center(
+                        child: Text("Error Occured"),
+                      );
+                    }
+                  }),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget coinDetails(CoinDetailsModel model) {
+    return ListTile(
+      leading: Image.network(
+        model.image,
+      ),
+      title: Text(
+        "BitCoin",
+        style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+      ),
+      trailing: RichText(
+        textAlign: TextAlign.end,
+        text: TextSpan(
+          text: "15244.98\n",
+          style: TextStyle(
+            fontSize: 17,
+            fontWeight: FontWeight.w500,
+            color: Colors.black,
+          ),
+          children: [
+            TextSpan(
+              text: "3.02%",
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 17,
+                color: Colors.red,
+              ),
+            ),
+          ],
+        ),
+
       ),
     );
   }
